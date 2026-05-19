4f9a768d75aeeb29a0436084d453eab4df027a18
akashnegi62 <akashsing215@gmail.com>
2026-05-19 17:48:43 +0530

    Test

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,7 +2,7 @@ import Booking from "@/components/Home/Booking";
 import Categories from "@/components/Home/Categories";
 import Favourite from "@/components/Home/Favourite";
 import Hero from "@/components/Home/Hero";
-import Itinerary from "@/components/Home/Itineraries";
+import Itinerary from "@/components/Home/Itinerary";
 import Appsec from "@/components/Home/Appsec";
 import LatestNews from "@/components/Home/LatestNews";
 import PartnersMarquee from "@/components/Home/PartnersMarquee";
